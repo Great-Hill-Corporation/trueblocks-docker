550c87208eb95cb6b05c6e60c796ae39edfe7cce
tjayrush <jrush@quickblocks.io>
2021-06-13 00:00:29 -0400

    Trying to get it to work

--- trueblocks.entrypoint.sh
+++ trueblocks.entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# write the RPC provider to quickBlocks.toml
 sed -i "s|rpcProvider =.*|rpcProvider = \"$RPC_PROVIDER\"|" /root/.quickBlocks/quickBlocks.toml
 
 export DOCKER_MODE=true
-# chifra scrape --sleep 14 --daemon &
-# forever /root/trueblocks-explorer/api/server.js 80
 chifra serve
